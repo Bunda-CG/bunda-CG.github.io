cdb47793aefa73f8bc0faa2aecfe6fa3d7039709
Thada Srisaluaikun <gnar875@gmail.com>
2024-05-05 00:30:09 +0700

    Add Updating Frame
    - Add updating box to show real-time status.
    - Add `refresh()` for re-drawing.
    - Showing updating noise.

--- elementControl.js
+++ elementControl.js
@@ -5,25 +5,39 @@ export function print(text) {
     pBox.innerHTML += text + "<br>";
 }
 
+export function updatePrint(text) {
+    const updateBox = document.getElementById(cf.CONSOLE_UPDATE);
+    updateBox.innerHTML = text;
+}
+
 export class Graphic {
 
     constructor() {
+        // canvas
         this.canvas = document.getElementById(cf.CANVAS_NAME);
         this.width = this.canvas.clientWidth;
         this.height = this.canvas.clientHeight;
-
+        
+        // context
         this.ctx = this.canvas.getContext("2d");
-        this.image = this.ctx.getImageData(0, 0, this.canvas.clientWidth, this.canvas.clientHeight);
-
+        
+        // image
+        this.image = this.ctx.getImageData(0, 0, this.width, this.height);
         this.red = cf.RED;
         this.green = cf.GREEN;
         this.blue = cf.BLUE;
         this.alpha = cf.ALPHA;
+
+        // background
+        this.bgRed = this.image.data[0];
+        this.bgGreen = this.image.data[1];
+        this.bgBlue = this.image.data[2];
+        this.bgAlpha = this.image.data[3];
     }
 
     setPixel(x, y) {
         // gonna use simple math coordinate, so flip it.
-        let flippedY = this.height - y
+        let flippedY = this.height - y;
         let pixIdx = (this.width * flippedY + x) * 4;
         // Red
         this.image.data[pixIdx + 0] = this.red;
@@ -37,12 +51,29 @@ export class Graphic {
         // Alpha
         this.image.data[pixIdx + 3] = this.alpha;
     }
-
+    
     setColor(r, g, b, a) {
         this.red = r;
         this.green = g;
         this.blue = b;
         this.alpha = a;
+    }
+
+    setBgColor(r, g, b, a) {
+        this.bgRed = r;
+        this.bgGreen = g;
+        this.bgBlue = b;
+        this.bgAlpha = a;
+    }
+
+    refresh() {
+        // put clean background
+        for (let i = 0; i < this.image.data.length; i+=4) {
+            this.image.data[i + 0] = this.bgRed;
+            this.image.data[i + 1] = this.bgGreen;
+            this.image.data[i + 2] = this.bgBlue;
+            this.image.data[i + 3] = this.bgAlpha;
+        }
     }
 
     update() {
