2d0145903d885f698633e0a29cda136f17afdfbd
Thada Srisaluaikun <gnar875@gmail.com>
2024-05-14 18:53:41 +0700

    Update scene7.js

--- src/scene7.js
+++ src/scene7.js
@@ -12,13 +12,13 @@ export class StareScene extends Scene {
     // #region obj
     // #region pepeBody
     //pepe body 150 px, c = (550,325)
-    this.pepebodytop = new anim.cubicBezierSpline(
+    this.pepeBodyTop = new anim.cubicBezierSpline(
       ...[400, 280],
       ...[400, 480],
       ...[700, 480],
       ...[700, 280]
     );
-    this.pepebodybottom = new anim.cubicBezierSpline(
+    this.pepeBodyBottom = new anim.cubicBezierSpline(
       ...[400, 280],
       ...[400, 80],
       ...[700, 80],
@@ -28,13 +28,13 @@ export class StareScene extends Scene {
 
     // #region pepeEye
     //pepe left eye 15 30 px, c = (480,275) 40
-    this.pepelefteyetop = new anim.cubicBezierSpline(
+    this.pepeLeftEyeTop = new anim.cubicBezierSpline(
       ...[465, 230],
       ...[465, 270],
       ...[495, 270],
       ...[495, 230]
     );
-    this.pepelefteyebottom = new anim.cubicBezierSpline(
+    this.pepeLeftEyeBottom = new anim.cubicBezierSpline(
       ...[465, 230],
       ...[465, 190],
       ...[495, 190],
@@ -42,13 +42,13 @@ export class StareScene extends Scene {
     );
 
     //pepe right eye 15 30 px, c = (550,285) 40
-    this.peperighteyetop = new anim.cubicBezierSpline(
+    this.pepeRightEyeTop = new anim.cubicBezierSpline(
       ...[535, 230],
       ...[535, 270],
       ...[565, 270],
       ...[565, 230]
     );
-    this.peperightteyebottom = new anim.cubicBezierSpline(
+    this.pepeRightEyeBottom = new anim.cubicBezierSpline(
       ...[535, 230],
       ...[535, 190],
       ...[565, 190],
@@ -58,13 +58,13 @@ export class StareScene extends Scene {
 
     // #region janitorBody
     //janitor body 150 px, c = (170,280)
-    this.janitorbodytop = new anim.cubicBezierSpline(
+    this.janitorBodyTop = new anim.cubicBezierSpline(
       ...[50, 280],
       ...[50, 480],
       ...[350, 480],
       ...[350, 280]
     );
-    this.janitorbodybottom = new anim.cubicBezierSpline(
+    this.janitorBodyBottom = new anim.cubicBezierSpline(
       ...[50, 280],
       ...[50, 80],
       ...[350, 80],
@@ -74,13 +74,13 @@ export class StareScene extends Scene {
 
     // #region janitorEye
     //janitor left eye 15 30 px, c = (165,245) 40
-    this.janitorlefteyetop = new anim.cubicBezierSpline(
+    this.janitorLeftEyeTop = new anim.cubicBezierSpline(
       ...[150, 245],
       ...[150, 285],
       ...[180, 285],
       ...[180, 245]
     );
-    this.janitorlefteyebottom = new anim.cubicBezierSpline(
+    this.janitorLeftEyeBottom = new anim.cubicBezierSpline(
       ...[150, 245],
       ...[150, 205],
       ...[180, 205],
@@ -88,18 +88,42 @@ export class StareScene extends Scene {
     );
 
     //janitor right eye 15 30 px, c = (250,250) 40
-    this.janitorrighteyetop = new anim.cubicBezierSpline(
+    this.janitorRightEyeTop = new anim.cubicBezierSpline(
       ...[235, 250],
       ...[235, 290],
       ...[265, 290],
       ...[265, 250]
     );
-    this.janitorrightteyebottom = new anim.cubicBezierSpline(
+    this.janitorRightEyeBottom = new anim.cubicBezierSpline(
       ...[235, 250],
       ...[235, 210],
       ...[265, 210],
       ...[265, 250]
     );
+    // #endregion
+
+    // #region seat
+    this.seat = new anim.splineChain(
+      ...[100, 0],
+      ...[140, 25],
+      ...[200, 50],
+      ...[373, 50]
+    );
+    this.seat.addPoint(700, 0);
+    this.pattern = new anim.splineChain(
+      ...[150, 0],
+      ...[150, 10],
+      ...[160, 10],
+      ...[165, 5]
+    );
+    this.pattern.addPoint(...[190, 13], ...[171, 6]);
+    this.pattern.addPoint(...[219, 3], ...[200, 26]);
+    this.pattern.addPoint(...[275, 18], ...[238, 11]);
+    this.pattern.addPoint(...[374, 19], ...[321, 38]);
+    this.pattern.addPoint(...[417, 18], ...[399, 32]);
+    this.pattern.addPoint(...[486, 14], ...[456, 31]);
+    this.pattern.addPoint(...[572, 18], ...[553, 46]);
+    this.pattern.addPoint(...[599, 0], ...[583, -2]);
     // #endregion
     // #endregion
   }
@@ -123,9 +147,19 @@ export class StareScene extends Scene {
 
   makeAnimate() {
     // #region anim
-    //move up pepe eye
+
+    // #region eyesUp
+    // #region pepeEyes
     this.addKeyframe(
-      this.pepelefteyetop,
+      this.pepeLeftEyeTop,
+      5000,
+      anim.translation,
+      fn.SIGMOID,
+      0,
+      80
+    );
+    this.addKeyframe(
+      this.pepeLeftEyeBottom,
       5000,
       anim.translation,
       fn.SIGMOID,
@@ -134,7 +168,7 @@ export class StareScene extends Scene {
     );
 
     this.addKeyframe(
-      this.pepelefteyebottom,
+      this.pepeRightEyeTop,
       5000,
       anim.translation,
       fn.SIGMOID,
@@ -142,7 +176,18 @@ export class StareScene extends Scene {
       80
     );
     this.addKeyframe(
-      this.peperighteyetop,
+      this.pepeRightEyeBottom,
+      5000,
+      anim.translation,
+      fn.SIGMOID,
+      0,
+      80
+    );
+    // #endregion
+
+    // #region janitorEyes
+    this.addKeyframe(
+      this.janitorLeftEyeBottom,
       5000,
       anim.translation,
       fn.SIGMOID,
@@ -150,7 +195,7 @@ export class StareScene extends Scene {
       80
     );
     this.addKeyframe(
-      this.peperightteyebottom,
+      this.janitorLeftEyeTop,
       5000,
       anim.translation,
       fn.SIGMOID,
@@ -158,44 +203,29 @@ export class StareScene extends Scene {
       80
     );
 
-    // move up janitor eye
     this.addKeyframe(
-      this.janitorlefteyebottom,
+      this.janitorRightEyeTop,
       5000,
       anim.translation,
       fn.SIGMOID,
       0,
       80
     );
+    this.addKeyframe(
+      this.janitorRightEyeBottom,
+      5000,
+      anim.translation,
+      fn.SIGMOID,
+      0,
+      80
+    );
+    // #endregion
+    // #endregion
 
+    // #region eyesStare
+    // #region pepeEyes
     this.addKeyframe(
-      this.janitorlefteyetop,
-      5000,
-      anim.translation,
-      fn.SIGMOID,
-      0,
-      80
-    );
-    this.addKeyframe(
-      this.janitorrighteyetop,
-      5000,
-      anim.translation,
-      fn.SIGMOID,
-      0,
-      80
-    );
-    this.addKeyframe(
-      this.janitorrightteyebottom,
-      5000,
-      anim.translation,
-      fn.SIGMOID,
-      0,
-      80
-    );
-
-    //move left pepe eye
-    this.addKeyframe(
-      this.pepelefteyetop,
+      this.pepeLeftEyeTop,
       10000,
       anim.translation,
       fn.SIGMOID,
@@ -204,23 +234,7 @@ export class StareScene extends Scene {
     );
 
     this.addKeyframe(
-      this.pepelefteyebottom,
-      10000,
-      anim.translation,
-      fn.SIGMOID,
-      -40,
-      0
-    );
-    this.addKeyframe(
-      this.peperighteyetop,
-      10000,
-      anim.translation,
-      fn.SIGMOID,
-      -40,
-      0
-    );
-    this.addKeyframe(
-      this.peperightteyebottom,
+      this.pepeLeftEyeBottom,
       10000,
       anim.translation,
       fn.SIGMOID,
@@ -228,9 +242,28 @@ export class StareScene extends Scene {
       0
     );
 
-    // move right janitor eye
     this.addKeyframe(
-      this.janitorlefteyebottom,
+      this.pepeRightEyeTop,
+      10000,
+      anim.translation,
+      fn.SIGMOID,
+      -40,
+      0
+    );
+
+    this.addKeyframe(
+      this.pepeRightEyeBottom,
+      10000,
+      anim.translation,
+      fn.SIGMOID,
+      -40,
+      0
+    );
+    // #endregion
+
+    // #region janitorEyes
+    this.addKeyframe(
+      this.janitorLeftEyeBottom,
       10000,
       anim.translation,
       fn.SIGMOID,
@@ -239,29 +272,400 @@ export class StareScene extends Scene {
     );
 
     this.addKeyframe(
-      this.janitorlefteyetop,
+      this.janitorLeftEyeTop,
       10000,
       anim.translation,
       fn.SIGMOID,
       40,
+      0
+    );
+
+    this.addKeyframe(
+      this.janitorRightEyeTop,
+      10000,
+      anim.translation,
+      fn.SIGMOID,
+      40,
+      0
+    );
+
+    this.addKeyframe(
+      this.janitorRightEyeBottom,
+      10000,
+      anim.translation,
+      fn.SIGMOID,
+      40,
+      0
+    );
+    // #endregion
+    // #endregion
+
+    // #region pepeClose
+    this.addKeyframe(
+      this.pepeLeftEyeBottom,
+      10500,
+      anim.scaling,
+      fn.SIGMOID,
+      1,
+      0.1,
+      473,
+      310
+    );
+    this.addKeyframe(
+      this.pepeLeftEyeTop,
+      10500,
+      anim.scaling,
+      fn.SIGMOID,
+      1,
+      0.1,
+      473,
+      310
+    );
+    this.addKeyframe(
+      this.pepeRightEyeBottom,
+      10500,
+      anim.scaling,
+      fn.SIGMOID,
+      1,
+      0.1,
+      473,
+      310
+    );
+    this.addKeyframe(
+      this.pepeRightEyeTop,
+      10500,
+      anim.scaling,
+      fn.SIGMOID,
+      1,
+      0.1,
+      473,
+      310
+    );
+    // #endregion
+
+    // #region pepeOpen
+    this.addKeyframe(
+      this.pepeLeftEyeBottom,
+      11000,
+      anim.scaling,
+      fn.SIGMOID,
+      1,
+      10,
+      473,
+      310
+    );
+    this.addKeyframe(
+      this.pepeLeftEyeTop,
+      11000,
+      anim.scaling,
+      fn.SIGMOID,
+      1,
+      10,
+      473,
+      310
+    );
+    this.addKeyframe(
+      this.pepeRightEyeBottom,
+      11000,
+      anim.scaling,
+      fn.SIGMOID,
+      1,
+      10,
+      473,
+      310
+    );
+    this.addKeyframe(
+      this.pepeRightEyeTop,
+      11000,
+      anim.scaling,
+      fn.SIGMOID,
+      1,
+      10,
+      473,
+      310
+    );
+    // #endregion
+
+    // #region pepeClose
+    this.addKeyframe(
+      this.pepeLeftEyeBottom,
+      11500,
+      anim.scaling,
+      fn.SIGMOID,
+      1,
+      0.1,
+      473,
+      310
+    );
+    this.addKeyframe(
+      this.pepeLeftEyeTop,
+      11500,
+      anim.scaling,
+      fn.SIGMOID,
+      1,
+      0.1,
+      473,
+      310
+    );
+    this.addKeyframe(
+      this.pepeRightEyeBottom,
+      11500,
+      anim.scaling,
+      fn.SIGMOID,
+      1,
+      0.1,
+      473,
+      310
+    );
+    this.addKeyframe(
+      this.pepeRightEyeTop,
+      11500,
+      anim.scaling,
+      fn.SIGMOID,
+      1,
+      0.1,
+      473,
+      310
+    );
+    // #endregion
+
+    // #region pepeOpen
+    this.addKeyframe(
+      this.pepeLeftEyeBottom,
+      12000,
+      anim.scaling,
+      fn.SIGMOID,
+      1,
+      10,
+      473,
+      310
+    );
+    this.addKeyframe(
+      this.pepeLeftEyeTop,
+      12000,
+      anim.scaling,
+      fn.SIGMOID,
+      1,
+      10,
+      473,
+      310
+    );
+    this.addKeyframe(
+      this.pepeRightEyeBottom,
+      12000,
+      anim.scaling,
+      fn.SIGMOID,
+      1,
+      10,
+      473,
+      310
+    );
+    this.addKeyframe(
+      this.pepeRightEyeTop,
+      12000,
+      anim.scaling,
+      fn.SIGMOID,
+      1,
+      10,
+      473,
+      310
+    );
+    // #endregion
+
+    // #region janitorStare
+    this.addKeyframe(
+      this.janitorLeftEyeBottom,
+      12000,
+      anim.scaling,
+      fn.SIGMOID,
+      1,
+      0.1,
+      247,
+      329
+    );
+    this.addKeyframe(
+      this.janitorLeftEyeTop,
+      12000,
+      anim.scaling,
+      fn.SIGMOID,
+      1,
+      0.1,
+      247,
+      329
+    );
+    this.addKeyframe(
+      this.janitorRightEyeBottom,
+      12000,
+      anim.scaling,
+      fn.SIGMOID,
+      1,
+      0.1,
+      247,
+      329
+    );
+    this.addKeyframe(
+      this.janitorRightEyeTop,
+      12000,
+      anim.scaling,
+      fn.SIGMOID,
+      1,
+      0.1,
+      247,
+      329
+    );
+    // #endregion
+
+    // #region pepeLookRight
+    this.addKeyframe(
+      this.pepeLeftEyeBottom,
+      12500,
+      anim.translation,
+      fn.SIGMOID,
+      149,
       0
     );
     this.addKeyframe(
-      this.janitorrighteyetop,
-      10000,
+      this.pepeLeftEyeTop,
+      12500,
       anim.translation,
       fn.SIGMOID,
-      40,
+      149,
       0
     );
     this.addKeyframe(
-      this.janitorrightteyebottom,
-      10000,
+      this.pepeRightEyeBottom,
+      12500,
       anim.translation,
       fn.SIGMOID,
-      40,
+      149,
       0
     );
+    this.addKeyframe(
+      this.pepeRightEyeTop,
+      12500,
+      anim.translation,
+      fn.SIGMOID,
+      149,
+      0
+    );
+    // #endregion
+
+    // #region pepeLookLeft
+    this.addKeyframe(
+      this.pepeLeftEyeBottom,
+      13000,
+      anim.translation,
+      fn.SIGMOID,
+      -149,
+      0
+    );
+    this.addKeyframe(
+      this.pepeLeftEyeTop,
+      13000,
+      anim.translation,
+      fn.SIGMOID,
+      -149,
+      0
+    );
+    this.addKeyframe(
+      this.pepeRightEyeBottom,
+      13000,
+      anim.translation,
+      fn.SIGMOID,
+      -149,
+      0
+    );
+    this.addKeyframe(
+      this.pepeRightEyeTop,
+      13000,
+      anim.translation,
+      fn.SIGMOID,
+      -149,
+      0
+    );
+    // #endregion
+
+    // #region pepeRotateEyes
+    this.addKeyframe(
+      this.pepeLeftEyeBottom,
+      17000,
+      anim.rotation,
+      fn.SIGMOID,
+      -2160,
+      473,
+      299
+    );
+    this.addKeyframe(
+      this.pepeLeftEyeTop,
+      17000,
+      anim.rotation,
+      fn.SIGMOID,
+      -2160,
+      473,
+      299
+    );
+    this.addKeyframe(
+      this.pepeRightEyeBottom,
+      17000,
+      anim.rotation,
+      fn.SIGMOID,
+      -2160,
+      473,
+      299
+    );
+    this.addKeyframe(
+      this.pepeRightEyeTop,
+      17000,
+      anim.rotation,
+      fn.SIGMOID,
+      -2160,
+      473,
+      299
+    );
+    // #endregion
+
+    // #region pepeShrinkEyes
+    this.addKeyframe(
+      this.pepeLeftEyeBottom,
+      18000,
+      anim.scaling,
+      fn.SIGMOID,
+      0.3,
+      0.3,
+      474,
+      298
+    );
+    this.addKeyframe(
+      this.pepeLeftEyeTop,
+      18000,
+      anim.scaling,
+      fn.SIGMOID,
+      0.3,
+      0.3,
+      474,
+      298
+    );
+    this.addKeyframe(
+      this.pepeRightEyeBottom,
+      18000,
+      anim.scaling,
+      fn.SIGMOID,
+      0.3,
+      0.3,
+      474,
+      298
+    );
+    this.addKeyframe(
+      this.pepeRightEyeTop,
+      18000,
+      anim.scaling,
+      fn.SIGMOID,
+      0.3,
+      0.3,
+      474,
+      298
+    );
+    // #endregion
     // #endregion
   }
 }
