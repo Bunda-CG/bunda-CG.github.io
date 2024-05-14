9312ec2b0bf7e639d5bfdee295d57e68de3ab062
Thada Srisaluaikun <gnar875@gmail.com>
2024-05-14 21:35:56 +0700

    Split File

--- src/scene2.js
+++ src/scene2.js
@@ -2,7 +2,7 @@ import * as anim from "./helperTools/animation.js";
 import * as fn from "./function.js";
 import { Scene } from "./scenes.js";
 
-export class TittaScene extends Scene {
+export class TittaScene extends Scene{
   static START_AT = 0;
   static END_AT = 3000000;
   constructor() {
@@ -12,162 +12,113 @@ export class TittaScene extends Scene {
 
     //outer toilet : circle radius = 250 px, c = (400,300)
     this.outerToiletTop = new anim.cubicBezierSpline(
-      150,
-      300,
-      150,
-      633,
-      650,
-      633,
-      650,
-      300
+        150,300,
+        150,633,
+        650,633,
+        650,300
     );
 
-    this.outerToiletBottom = new anim.cubicBezierSpline(
-      150,
-      300,
-      150,
-      -33,
-      650,
-      -33,
-      650,
-      300
-    );
+   this.outerToiletBottom = new anim.cubicBezierSpline(
+        150,300,
+        150,-33,
+        650,-33,
+        650,300
+   );
     //inner toilet : circle radius = 200 px, c = (400,300)
     this.innerToiletTop = new anim.cubicBezierSpline(
-      200,
-      300,
-      200,
-      567,
-      600,
-      567,
-      600,
-      300
+        200,300,
+        200,567,
+        600,567,
+        600,300
     );
 
     this.innerToiletBottom = new anim.cubicBezierSpline(
-      200,
-      300,
-      200,
-      33,
-      600,
-      33,
-      600,
-      300
+        200,300,
+        200,33,
+        600,33,
+        600,300
     );
 
     //water surface line : circle radius = 100 px, c = (400,300)
     this.innerWaterTop = new anim.cubicBezierSpline(
-      300,
-      300,
-      300,
-      433,
-      500,
-      433,
-      500,
-      300
+        300,300,
+        300,433,
+        500,433,
+        500,300
     );
 
     this.innerWaterBottom = new anim.cubicBezierSpline(
-      300,
-      300,
-      300,
-      167,
-      500,
-      167,
-      500,
-      300
+        300,300,
+        300,167,
+        500,167,
+        500,300
     );
 
     //fecus no.1
     this.fecus1Top = new anim.cubicBezierSpline(
-      386,
-      301,
-      343,
-      402,
-      451,
-      430,
-      465,
-      425
+        386,301,
+        343,402,
+        451,430,
+        465,425
     );
 
     this.fecus1Bottom = new anim.cubicBezierSpline(
-      386,
-      301,
-      400,
-      450,
-      478,
-      350,
-      465,
-      425
+        386,301,
+        400,450,
+        478,350,
+        465,425
     );
 
     //fecus no.2
     this.fecus2Top = new anim.cubicBezierSpline(
-      340,
-      200,
-      357,
-      317,
-      426,
-      341,
-      510,
-      250
+        340,200,
+        357,317,
+        426,341,
+        510,250
     );
 
     this.fecus2BottomLeft = new anim.cubicBezierSpline(
-      416,
-      265,
-      390,
-      245,
-      355,
-      190,
-      340,
-      200
+        416,265,
+        390,245,
+        355,190,
+        340,200
     );
 
     this.fecus2BottomRight = new anim.cubicBezierSpline(
-      416,
-      265,
-      435,
-      270,
-      530,
-      215,
-      510,
-      250
+        416,265,
+        435,270,
+        530,215,
+        510,250
     );
 
     //fecus no.3
     this.fecus3 = new anim.incompletepolygon();
-    this.fecus3.addPoint(360, 300);
-    this.fecus3.addPoint(330, 270);
-    this.fecus3.addPoint(310, 317);
-    this.fecus3.addPoint(338, 380);
-    this.fecus3.addPoint(370, 330);
-    this.fecus3.addPoint(360, 300);
+    this.fecus3.addPoint(360,300);
+    this.fecus3.addPoint(330,270);
+    this.fecus3.addPoint(310,317);
+    this.fecus3.addPoint(338,380);
+    this.fecus3.addPoint(370,330);
+    this.fecus3.addPoint(360,300);
 
     //water wave 1
     this.wave1 = new anim.cubicBezierSpline(
-      348,
-      321,
-      341,
-      353,
-      400,
-      387,
-      420,
-      370
+        348,321,
+        341,353,
+        400,387,
+        420,370
     );
 
     //water wave 2
     this.wave2 = new anim.cubicBezierSpline(
-      460,
-      325,
-      450,
-      280,
-      470,
-      283,
-      400,
-      230
+        460,325,
+        450,280,
+        470,283,
+        400,230
     );
-  }
+
+   }
+
+   
 
   addKeyframe(object, endAt, transform, moveFunc, ...args) {
     super.preventStart(
@@ -188,27 +139,34 @@ export class TittaScene extends Scene {
 
   makeAnimate() {
     this.addKeyframe(
-      this.wave1,
-      20000,
-      anim.rotation,
-      fn.SIGMOID,
-      -1600,
-      400,
-      300
+        this.wave1,
+        20000,
+        anim.rotation,
+        fn.SIGMOID,
+        -1600,
+        400,
+        300
     );
     this.addKeyframe(
-      this.wave2,
-      20000,
-      anim.rotation,
-      fn.SIGMOID,
-      -1600,
-      400,
-      300
+        this.wave2,
+        20000,
+        anim.rotation,
+        fn.SIGMOID,
+        -1600,
+        400,
+        300
     );
 
-    //20s = 6s shear + 8s rotate + 6s scaling
+  //20s = 6s shear + 8s rotate + 6s scaling
     //shearing
-    this.addKeyframe(this.fecus1Top, 6000, anim.shearx, fn.SIGMOID, 0.5, 370);
+    this.addKeyframe(
+      this.fecus1Top,
+      6000,
+      anim.shearx,
+      fn.SIGMOID,
+      0.5,
+      370
+    );
 
     this.addKeyframe(
       this.fecus1Bottom,
@@ -219,7 +177,14 @@ export class TittaScene extends Scene {
       370
     );
 
-    this.addKeyframe(this.fecus2Top, 6000, anim.shearx, fn.SIGMOID, -0.12, 220);
+    this.addKeyframe(
+      this.fecus2Top,
+      6000,
+      anim.shearx,
+      fn.SIGMOID,
+      -0.12,
+      220
+    );
 
     this.addKeyframe(
       this.fecus2BottomLeft,
@@ -239,7 +204,16 @@ export class TittaScene extends Scene {
       220
     );
 
-    this.addKeyframe(this.fecus3, 6000, anim.sheary, fn.SIGMOID, -1, 318);
+    this.addKeyframe(
+      this.fecus3,
+      6000,
+      anim.sheary,
+      fn.SIGMOID,
+      -1,
+      318
+    );
+
+
 
     //rotating
     this.addKeyframe(
@@ -302,14 +276,15 @@ export class TittaScene extends Scene {
       300
     );
 
+
+
     //scaling
     this.addKeyframe(
       this.fecus1Top,
       20000,
       anim.scaling,
       fn.SIGMOID,
-      0.25,
-      0.25,
+      0.25,0.25,
       400,
       300
     );
@@ -319,8 +294,7 @@ export class TittaScene extends Scene {
       20000,
       anim.scaling,
       fn.SIGMOID,
-      0.25,
-      0.25,
+      0.25,0.25,
       400,
       300
     );
@@ -330,8 +304,7 @@ export class TittaScene extends Scene {
       20000,
       anim.scaling,
       fn.SIGMOID,
-      0.25,
-      0.25,
+      0.25,0.25,
       400,
       300
     );
@@ -341,8 +314,7 @@ export class TittaScene extends Scene {
       20000,
       anim.scaling,
       fn.SIGMOID,
-      0.25,
-      0.25,
+      0.25,0.25,
       400,
       300
     );
@@ -352,8 +324,7 @@ export class TittaScene extends Scene {
       20000,
       anim.scaling,
       fn.SIGMOID,
-      0.25,
-      0.25,
+      0.25,0.25,
       400,
       300
     );
@@ -363,10 +334,10 @@ export class TittaScene extends Scene {
       20000,
       anim.scaling,
       fn.SIGMOID,
-      0.25,
-      0.25,
+      0.25,0.25,
       400,
       300
     );
   }
+
 }
